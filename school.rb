e2e33030306c63c0466ddf6c0f384c55ae5afcd1
Buyaki <rittahbuyaki@gmail.com>
2022-03-02 15:08:03 +0300

    Add class Person in the school.rb file and the linters files

---
+++ school.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize(name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def of_age?
+    @age >= 18
+  end
+
+  # def can_use_services?
+  #   if is_of_age? == true && parent_permission = true
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
+end
